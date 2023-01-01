177d2176b541cc3b865bd2c126709be4c3634e53
Oleg Kravchenko <kitaist@kitaist.com>
2023-01-01 09:21:00 -0800

    color names

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,11 +1,35 @@
-export const Form = () => {
+import colorNames from 'colornames'
+
+export const Form = ({
+  colorValue,
+  setColorValue,
+  setHexValue,
+  isDarkText,
+  setIsDarkText,
+}) => {
+  const changeTextColor = () => setIsDarkText(!isDarkText)
+
+  const addColorValues = (e) => {
+    setColorValue(e.target.value)
+    setHexValue(colorNames(e.target.value) || '')
+  }
+
   return (
-    <form>
+    <form onSubmit={(e) => e.preventDefault()}>
       <label>Add Color Name</label>
 
-      <input type="text" placeholder="Add color name" required />
+      <input
+        type="text"
+        autoFocus
+        placeholder="Add color name"
+        required
+        value={colorValue}
+        onChange={addColorValues}
+      />
 
-      <button type="button">Toggle color</button>
+      <button type="button" onClick={changeTextColor}>
+        Toggle color
+      </button>
     </form>
   )
 }
--- src/components/Square/Square.jsx
+++ src/components/Square/Square.jsx
@@ -1,7 +1,17 @@
-export const Square = () => {
+export const Square = ({colorValue, hexValue, isDarkText}) => {
+  const style = {
+    backgroundColor: colorValue,
+    color: isDarkText ? '#000' : '#fff',
+  }
+
   return (
-    <section className="square">
-      <p>Empty Value</p>
+    <section className="square" style={style}>
+      <p>{colorValue ? colorValue : 'Empty Color Value'}</p>
+      <p>{hexValue ? hexValue : null}</p>
     </section>
   )
+}
+
+Square.defaultProps = {
+  colorValue: 'Empty Color Value',
 }
