468f51fa7d312f29ec2861ab18290e7633c677c8
Oleg Kravchenko <kitaist@kitaist.com>
2023-01-01 08:23:21 -0800

    project starting files

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,11 @@
+export const Form = () => {
+  return (
+    <form>
+      <label>Add Color Name</label>
+
+      <input type="text" placeholder="Add color name" required />
+
+      <button type="button">Toggle color</button>
+    </form>
+  )
+}
---
+++ src/components/Square/Square.jsx
@@ -0,0 +1,7 @@
+export const Square = () => {
+  return (
+    <section className="square">
+      <p>Empty Value</p>
+    </section>
+  )
+}
