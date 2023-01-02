f329264fd3d5d39aad96f2b23a2a13d66b80ee85
Oleg Kravchenko <kitaist@kitaist.com>
2023-01-01 21:05:22 -0800

    project structure

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import Header from './Header'
+import Content from './Content'
+import Footer from './Footer'
+
+export const App = () => {
+  return (
+    <div className="App">
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  )
+}
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  const today = new Date()
+
+  return (
+    <footer>
+      <p>Copyright &copy; {today.getFullYear()}</p>
+    </footer>
+  )
+}
